547942ae5e6595e7b0543b0e7b372378a57bd40c
patrice012 <patricedev02@gmail.com>
2024-03-13 10:43:28 +0000

    add server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = require('./routes/index');
+
+const server = express();
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+server.use('/', router);
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
